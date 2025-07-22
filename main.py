5708ad6e494df5c4e7319bf252c5e5cf34dee95e
ashb155 <b.ashutosh198@gmail.com>
2025-07-23 00:20:01 +0530

    Added logistic regression model with TF-IDF for classification

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+
+train["text"] = train["Title"] + " " + train["Description"]
+test["text"] = test["Title"] + " " + test["Description"]
+
+X_train = train["text"]
+y_train = train["Class Index"]
+X_test = test["text"]
+y_test = test["Class Index"]
+
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train_vec, y_train)
+
+y_pred = model.predict(X_test_vec)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
